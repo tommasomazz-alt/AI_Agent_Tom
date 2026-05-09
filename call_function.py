187ca10be3e1b1db62f0ec5c16aa7003dffd8a60
tommasomazz-alt <tommaso.mazz@gmail.com>
2026-05-09 20:00:27 +0200

    added all the actual logic to the call_function function, updated main.py to actually do function calling

--- call_function.py
+++ call_function.py
@@ -1,8 +1,15 @@
 from google.genai import types
+
 from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_files_content
 from functions.run_python_file import schema_run_python_file
 from functions.write_file import schema_write_file
+
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+
 
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, 
@@ -10,3 +17,50 @@ available_functions = types.Tool(
                            schema_run_python_file,
                            schema_write_file],
 )
+
+# funzione per la task astratta di chiamare una delle nostre quattro funzioni e restituirne i risultati
+def call_function(function_call, verbose=False):
+
+    # volendo la funzione chiamata può essere None quindi la associo a una variabile e se è None restituisco errore
+    function_name = function_call.name or ""
+
+    if verbose:
+        print(f"Calling function: {function_name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_name}")
+    
+    function_map = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+
+    if function_name not in function_map:
+
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    
+    # faccio una copia shallow degli arguments poichè la working_directory la sovrascriveremo
+    args = dict(function_call.args) if function_call.args else {}
+
+    args["working_directory"] = "./calculator"
+
+    function_result = function_map[function_name](**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 # nel file .env abbiamo la chiave API per sfruttare Gemini AI, qui la carichiamo
 load_dotenv()
@@ -23,7 +23,7 @@ args = parser.parse_args()
 # diciamo di ricordare la lista di messaggi in modo che ogni risposta AI consideri tutta la conversazione
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-if api_key == None:
+if api_key is None:
     raise RuntimeError("API Key not found")
 
 client = genai.Client(api_key=api_key)
@@ -40,7 +40,7 @@ if response.usage_metadata is  None:
     raise RuntimeError("failed API request before printing the tokens consumed")
 
 # stampa i dettagli contestuali dell'interazione solo quando si usa il flag --verbose
-if args.verbose == True:
+if args.verbose:
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
@@ -51,5 +51,20 @@ if response.function_calls is None:
     print(response.text)
 
 elif isinstance(response.function_calls, list):
+    results = []
     for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call, verbose=args.verbose)
+
+        if not function_call_result.parts:
+            raise Exception("Error: .parts of call_function is empty")
+
+        if function_call_result.parts[0].function_response is None:
+            raise Exception("Error: not a function response")
+
+        if function_call_result.parts[0].function_response.response is None:
+            raise Exception("Error: not a response")
+        
+        results.append(function_call_result.parts[0])
+
+        if args.verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
